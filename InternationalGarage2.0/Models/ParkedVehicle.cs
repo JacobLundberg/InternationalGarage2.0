b940526c996c9cc4c0d77f310ca46e4902128009
pp33gt <mapundi@outlook.com>
2019-01-25 16:21:51 +0100

    split parkedvehicle into 2 files

--- InternationalGarage2.0/Models/ParkedVehicle.cs
+++ InternationalGarage2.0/Models/ParkedVehicle.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace InternationalGarage2_0.Models
 {
-    public enum VehicleType
-    {
-        Unknown,
-        Type1
-    }
 
     public class ParkedVehicle
     {
