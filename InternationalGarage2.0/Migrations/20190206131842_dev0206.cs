dfbb999ece8b0854b1ab7041ce101bdad0c11408
hanwen zhang <hanwen8102@outlook.com>
2019-02-06 14:41:34 +0100

    Entries to member and vehicletype were done.

---
+++ InternationalGarage2.0/Migrations/20190206131842_dev0206.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternationalGarage2._0.Migrations
+{
+    public partial class dev0206 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
